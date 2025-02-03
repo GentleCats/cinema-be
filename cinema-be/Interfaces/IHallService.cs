0f81477fdd0bd6ec603f20743ea6cc10127b3e53
SamuraiLevel100 <127427687+SamuraiLevel100@users.noreply.github.com>
2025-02-03 15:21:29 +0200

    Writing controllers for Hall

--- cinema-be/Interfaces/IHallService.cs
+++ cinema-be/Interfaces/IHallService.cs
@@ -1,6 +1,13 @@
-﻿namespace cinema_be.Interfaces
+﻿using cinema_be.Entities;
+using cinema_be.Models.DTOs;
+
+namespace cinema_be.Interfaces
 {
     public interface IHallService
     {
+        IEnumerable<Hall> GetHalls();
+        Hall GetHallById(int id);
+        void Create(CreateHallDto hallDto);
+        void Delete(int id);
     }
 }
