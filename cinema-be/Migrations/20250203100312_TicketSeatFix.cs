d30f8c7c79d44a6b1f4150781036622fc3bdb5c4
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-03 12:12:34 +0200

    fixed ticket(seat field)

---
+++ cinema-be/Migrations/20250203100312_TicketSeatFix.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cinema_be.Migrations
+{
+    /// <inheritdoc />
+    public partial class TicketSeatFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seat",
+                table: "Halls");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Seat",
+                table: "Tickets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seat",
+                table: "Tickets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Seat",
+                table: "Halls",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Halls",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Seat",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Halls",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Seat",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Halls",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Seat",
+                value: 0);
+        }
+    }
+}
