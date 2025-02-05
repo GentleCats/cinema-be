1c7768270ac43c98db5c3a339748efbc90d0495c
max655 <147141510+max655@users.noreply.github.com>
2025-02-05 16:53:04 +0200

    feat: ticket controller

---
+++ cinema-be/Models/DTO/CreateTicketDto.cs
@@ -0,0 +1,9 @@
+﻿namespace cinema_be.Models.DTOs
+{
+    public class CreateTicketDto
+    {
+        public int HallId { get; set; }
+        public int SeatNumber { get; set; }
+        public decimal Price { get; set; }
+    }
+}
