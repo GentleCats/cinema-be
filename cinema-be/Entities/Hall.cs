161a4dc51840bb458e6b322a146003a3684c7663
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-07 15:38:09 +0200

    fixed conflicts

--- cinema-be/Entities/Hall.cs
+++ cinema-be/Entities/Hall.cs
@@ -7,10 +7,7 @@ namespace cinema_be.Entities
     {
         [Key]
         public int Id { get; set; }
-        [Required, MaxLength(100)]
         public string Name { get; set; }
-        [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than 0")]
         public int Capacity { get; set; }
         public int Rows { get; set; }
         public int Cols { get; set; }
--- cinema-be/Entities/Ticket.cs
+++ cinema-be/Entities/Ticket.cs
@@ -16,12 +16,9 @@ namespace cinema_be.Entities
         [Required, ForeignKey(nameof(Session))]
         public int SessionId { get; set; }
         public int Seat { get; set; }
-<<<<<<< HEAD
         public int Row { get; set; }
 
         public int Col { get; set; }
-=======
->>>>>>> 48acea7b6042a4254e0b5e90058fadf2586579ff
         public User User { get; set; }
         public Session Session { get; set; }
     }
