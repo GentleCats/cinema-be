9e318495476b3eb86cb0b272b1d60f55d0e74c23
maxum2003la <maxum2003la@gmail.com>
2025-02-11 17:02:47 +0200

    feat: add method for getting ticket by sessionID

--- cinema-be/Models/DTO/CreateTicketDto.cs
+++ cinema-be/Models/DTO/CreateTicketDto.cs
@@ -5,6 +5,7 @@
         public int SessionId { get; set; }
         public int UserId { get; set; }
         public int Seat { get; set; }
-        
+        public int Cols { get; set; }
+        public int Rows { get; set; }
     }
 }
